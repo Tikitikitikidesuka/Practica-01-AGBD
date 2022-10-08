933e84f812bdf33d14319886b815bc0da82c8ffc
Tikitikitikidesuka <deesneakygerbil@gmail.com>
2022-10-08 20:06:19 +0200

    Añadir script `load_clientes.sql` para cargar la información de los clientes del fichero `Clientes.csv`

---
+++ scripts/load_clientes.sql
@@ -0,0 +1,16 @@
+USE PracABD1;
+
+/**
+ * Esta operación no funciona de serie
+ *
+ * En el tab de 'Conexión' de MySQL Workbench ve a 'Avanzado' y en el hueco de 'Otros' introduce:
+ * 	 OPT_LOCAL_INFILE=1
+ */
+
+LOAD DATA LOCAL INFILE '/home/miguel/Documents/projects/practica_01_agdb/Datos/Clientes.csv'
+	INTO TABLE clientes
+	CHARACTER SET utf8
+	FIELDS TERMINATED BY ','
+	LINES TERMINATED BY '\n'
+	(@ClienteID, DNI, Nombre, Apellidos, Genero, Direccion, Localidad, Provincia, CodPostal, Telefono, Canal, @FechaNacimiento, @FechaContacto, Email)
+	SET ClienteID = CAST(@ClienteID AS DECIMAL), FechaNacimiento = STR_TO_DATE(@FechaNacimiento, '%m/%d/%Y'), FechaContacto = STR_TO_DATE(@FechaContacto, '%m/%d/%Y');
