9adb7250cac516f8d4fa7fb06d469670c8ea3a61
Carlos Lafuente Sanz <carlos.lafuente.sanz@alumnos.upm.es>
2022-10-08 17:45:56 +0200

    Actualizar el fichero drop_physical.sql

--- drop_physical.sql
+++ drop_physical.sql
@@ -1,7 +1,7 @@
 USE PracADB1;
 
-DROP TABLESPACE `clientes`;
+DROP TABLESPACE `TBLS_clientes`;
 
-DROP TABLESPACE `juegos`;
+DROP TABLESPACE `TBLS_juegos`;
 
-DROP TABLESPACE `clientes_juegos`;
+DROP TABLESPACE `TBLS_clientes_juegos`;
