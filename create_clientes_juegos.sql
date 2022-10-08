7597457d40a1fa6cba0d20e3f30e2a8834082bcc
Tikitikitikidesuka <deesneakygerbil@gmail.com>
2022-10-08 18:42:47 +0200

    Juntar en un solo script la creación de las tablas `clientes`, `juegos` y `clientes_juegos`

--- create_clientes_juegos.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `clientes_juegos` (
-	ClienteID INT NOT NULL,
-    JuegoID INT NOT NULL,
-    FechaAlquiler DATE NOT NULL,
-    Comentarios VARCHAR(500),
-    PRIMARY KEY (ClienteID, JuegoID, FechaAlquiler)
-) TABLESPACE `TBLS_clientes_juegos` 
