6c81e3412fd3dac699a48268dee99c8d2dc8c742
Tikitikitikidesuka <deesneakygerbil@gmail.com>
2022-10-09 13:14:59 +0200

    Modificar script `load_clientes.sql` para permitir la carga desde ficheros

--- scripts/load_clientes.sql
+++ scripts/load_clientes.sql
@@ -6,11 +6,15 @@ USE PracABD1;
  * En el tab de 'Conexión' de MySQL Workbench ve a 'Avanzado' y en el hueco de 'Otros' introduce:
  * 	 OPT_LOCAL_INFILE=1
  */
+ 
+SET GLOBAL local_infile = TRUE;
 
-LOAD DATA LOCAL INFILE '/home/miguel/Documents/projects/practica_01_agdb/Datos/Clientes.csv'
+LOAD DATA LOCAL INFILE '/home/miguel/Documents/projects/practica_01_agdb/datos/Clientes.csv'
 	INTO TABLE clientes
 	CHARACTER SET utf8
 	FIELDS TERMINATED BY ','
 	LINES TERMINATED BY '\n'
 	(@ClienteID, DNI, Nombre, Apellidos, Genero, Direccion, Localidad, Provincia, CodPostal, Telefono, Canal, @FechaNacimiento, @FechaContacto, Email)
 	SET ClienteID = CAST(@ClienteID AS DECIMAL), FechaNacimiento = STR_TO_DATE(@FechaNacimiento, '%m/%d/%Y'), FechaContacto = STR_TO_DATE(@FechaContacto, '%m/%d/%Y');
+
+SET GLOBAL local_infile = FALSE;
