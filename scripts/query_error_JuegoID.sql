9e00389a9e7ff4d43467e4996b026ecb334c35ac
Tikitikitikidesuka <deesneakygerbil@gmail.com>
2022-10-10 10:13:05 +0200

    Añadir script que demuestre el error en los datos relacionado a los ids de juego

---
+++ scripts/query_error_JuegoID.sql
@@ -0,0 +1,48 @@
+/**
+ * Para investigar este error primero hay que librarse del que ocurre debido a
+ * la unicidad de los títulos de los juegos (Mirar README.md para más detalles)
+ */
+
+/**
+ * Mínimo y máximo `JuegoID` de los juegos de la MegaDrive
+ */
+ 
+SELECT MIN(JuegoID), MAX(JuegoID)
+	FROM juegos
+	WHERE JuegoID < 12000;
+    
+SELECT MIN(JuegoID), MAX(JuegoID)
+	FROM clientes_juegos
+    WHERE JuegoID < 12000;
+    
+/**
+ * Mínimo y máximo `JuegoID` de los juegos de la Nintendo
+ */
+ 
+SELECT MIN(JuegoID), MAX(JuegoID)
+	FROM juegos
+	WHERE JuegoID >= 12000 AND JuegoID < 15000;
+    
+SELECT MIN(JuegoID), MAX(JuegoID)
+	FROM clientes_juegos
+    WHERE JuegoID >= 12000 AND JuegoID < 15000;
+    
+/**
+ * Mínimo y máximo `JuegoID` de los juegos de la GameBoy
+ */
+ 
+SELECT MIN(JuegoID), MAX(JuegoID)
+	FROM juegos
+	WHERE JuegoID >= 15000;
+    
+SELECT MIN(JuegoID), MAX(JuegoID)
+	FROM clientes_juegos
+    WHERE JuegoID >= 15000; 
+
+/**
+ * Mínimo y máximo `JuegoID` de los juegos de la GameBoy
+ */
+
+SELECT *
+	FROM clientes_juegos LEFT JOIN juegos ON juegos.JuegoID = clientes_juegos.JuegoID
+    WHERE juegos.JuegoID IS NULL;
