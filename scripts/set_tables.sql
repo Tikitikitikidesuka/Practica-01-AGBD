141f949198b725c941907a894fed45f8c891783c
Tikitikitikidesuka <deesneakygerbil@gmail.com>
2022-10-20 19:25:59 +0200

    Cambiar espacios por tabs en el script `set_tables.sql`

--- scripts/set_tables.sql
+++ scripts/set_tables.sql
@@ -2,19 +2,19 @@ USE PracABD1;
 
 CREATE TABLE clientes (
 	ClienteID INT UNIQUE NOT NULL,
-    DNI VARCHAR(9) UNIQUE NOT NULL,
-    Nombre VARCHAR(20) NOT NULL,
-    Apellidos VARCHAR(30) NOT NULL,
-    Genero ENUM('H', 'M'), 
-    Direccion VARCHAR(60),
-    Localidad VARCHAR(50),
+	DNI VARCHAR(9) UNIQUE NOT NULL,
+	Nombre VARCHAR(20) NOT NULL,
+	Apellidos VARCHAR(30) NOT NULL,
+	Genero ENUM('H', 'M'), 
+	Direccion VARCHAR(60),
+	Localidad VARCHAR(50),
 	Provincia VARCHAR(30),
-    CodPostal VARCHAR(5),
-    Telefono VARCHAR(9),
-    Canal ENUM('desconocido', 'web', 'llamadas', 'visitas', 'contactos'),
-    FechaNacimiento DATE,
-    FechaContacto DATE,
-    Email VARCHAR(60)
+	CodPostal VARCHAR(5),
+	Telefono VARCHAR(9),
+	Canal ENUM('desconocido', 'web', 'llamadas', 'visitas', 'contactos'),
+	FechaNacimiento DATE,
+	FechaContacto DATE,
+	Email VARCHAR(60)
 ) TABLESPACE TBLS_clientes;
 
 CREATE TABLE juegos (
@@ -27,7 +27,7 @@ CREATE TABLE juegos (
 
 CREATE TABLE clientes_juegos (
 	ClienteID INT NOT NULL,
-    JuegoID INT NOT NULL,
-    FechaAlquiler DATE NOT NULL,
-    Comentarios VARCHAR(500)
+	JuegoID INT NOT NULL,
+	FechaAlquiler DATE NOT NULL,
+	Comentarios VARCHAR(500)
 ) TABLESPACE TBLS_clientes_juegos;
