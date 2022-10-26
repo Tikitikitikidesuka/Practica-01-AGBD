005ba9913b30063423128f8e1c6f074e9560c889
Tikitikitikidesuka <deesneakygerbil@gmail.com>
2022-10-26 22:20:23 +0200

    Añadir scrits de índices

---
+++ scripts/consultas/dropIndices.sql
@@ -0,0 +1,5 @@
+DROP INDEX nombre_cliente ON clientes;
+
+DROP INDEX apellidos_cliente ON clientes;
+
+DROP INDEX titulo_juego ON juegos;
---
+++ scripts/consultas/setIndices.sql
@@ -0,0 +1,5 @@
+CREATE INDEX nombre_cliente ON clientes (Nombre(20));
+
+CREATE INDEX apellidos_cliente ON clientes (Apellidos(30));
+
+CREATE INDEX titulo_juego ON juegos (Titulo(32));
