e2ebd22bb886b08122c29c501a509578d3453b5c
Tikitikitikidesuka <deesneakygerbil@gmail.com>
2022-10-28 20:52:58 +0200

    Repasar un pelin la memoria

--- scripts/load_curated_data.sql
+++ scripts/load_curated_data.sql
@@ -9,7 +9,7 @@ USE PracABD1;
 
 SET GLOBAL local_infile = TRUE;
 
-LOAD DATA LOCAL INFILE '/home/miguel/Documents/projects/practica_01_agdb/datos/datos_filtrados/clientes.csv'
+LOAD DATA LOCAL INFILE 'directorio_datos/clientes.csv'
 	INTO TABLE clientes
 	CHARACTER SET utf8mb4
 	FIELDS TERMINATED BY ','
@@ -17,7 +17,7 @@ LOAD DATA LOCAL INFILE '/home/miguel/Documents/projects/practica_01_agdb/datos/d
 	(@ClienteID, DNI, Nombre, Apellidos, Genero, Direccion, Localidad, Provincia, CodPostal, Telefono, Canal, @FechaNacimiento, @FechaContacto, Email)
 	SET ClienteID = CAST(@ClienteID AS DECIMAL), FechaNacimiento = STR_TO_DATE(@FechaNacimiento, '%Y-%m-%d'), FechaContacto = STR_TO_DATE(@FechaContacto, '%Y-%m-%d');
 
-LOAD DATA LOCAL INFILE '/home/miguel/Documents/projects/practica_01_agdb/datos/datos_filtrados/juegos.csv'
+LOAD DATA LOCAL INFILE 'directorio_datos/juegos.csv'
 	INTO TABLE juegos
 	CHARACTER SET utf8mb4
 	FIELDS TERMINATED BY ','
@@ -25,7 +25,7 @@ LOAD DATA LOCAL INFILE '/home/miguel/Documents/projects/practica_01_agdb/datos/d
 	(@JuegoID, Titulo, Consola, @Tamanio, Editor)
 	SET JuegoID = CAST(@JuegoID AS DECIMAL), Tamanio = CAST(@Tamanio AS DECIMAL);
 
-LOAD DATA LOCAL INFILE '/home/miguel/Documents/projects/practica_01_agdb/datos/datos_filtrados/clientes_juegos.csv'
+LOAD DATA LOCAL INFILE 'directorio_datos/clientes_juegos.csv'
 	INTO TABLE clientes_juegos
 	CHARACTER SET utf8mb4
 	FIELDS TERMINATED BY ','
